0d722532b72b0f24982e5ad8138ee7473c833d7c
Donald AC Martin <donald@donald-martin.ca>
2022-02-19 11:12:42 -0500

    Working splash screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+
+const routes: Routes = [
+  {path: '', component: SplashScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
