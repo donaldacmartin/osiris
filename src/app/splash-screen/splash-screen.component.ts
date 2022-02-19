0619385d92981a6ba1b624a66476e627a44e8bfd
Donald AC Martin <donald@donald-martin.ca>
2022-02-19 11:06:26 -0500

    Working sign-in button

--- src/app/splash-screen/splash-screen.component.ts
+++ src/app/splash-screen/splash-screen.component.ts
@@ -1,8 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SocialAuthService } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
 
 @Component({
   selector: 'app-splash-screen',
   templateUrl: './splash-screen.component.html',
   styleUrls: ['./splash-screen.component.css']
 })
-export class SplashScreenComponent {}
+export class SplashScreenComponent {
+
+  title: String = "Osiris";
+  subtitle: String = "YouTube Playlist Curator";
+
+  constructor(private authService: SocialAuthService) {}
+
+  signIn(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+}
