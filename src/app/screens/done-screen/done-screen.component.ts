8f9f89443f5f500f90d52ef1b47ba4fd514db591
Donald AC Martin <donald@donald-martin.ca>
2022-05-30 04:30:24 -0400

    Restart button

--- src/app/screens/done-screen/done-screen.component.ts
+++ src/app/screens/done-screen/done-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-done-screen',
@@ -6,8 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./done-screen.component.css'],
 })
 export class DoneScreenComponent {
+  private reloadUri = '/';
+
   title = 'All done!';
   message = 'Come back tomorrow for another playlist';
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  restart() {
+    this.router.navigate([this.reloadUri]);
+  }
+
 }
+
