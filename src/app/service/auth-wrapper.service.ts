fd7bd25de3bfc7fb6e0e6942b6a3f82b712088d1
Donald AC Martin <donald@donald-martin.ca>
2022-05-30 04:55:25 -0400

    LocalStorage of auth token

--- src/app/service/auth-wrapper.service.ts
+++ src/app/service/auth-wrapper.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { GoogleAuthProvider } from 'firebase/auth';
-import { OAuthCredential } from 'firebase/auth';
+import { GoogleAuthProvider, OAuthCredential } from 'firebase/auth';
 import { from, Observable } from 'rxjs';
 
 const SCOPE = 'https://www.googleapis.com/auth/youtube';
+const LOCAL_STORAGE_AUTH_NAME = '@osiris/auth-token';
+const LOCAL_STORAGE_AUTH_TTL = '@osis/auth-ttl';
 
 @Injectable({
   providedIn: 'root',
@@ -28,6 +29,7 @@ export class AuthWrapperService {
 
           if (credential.accessToken) {
             this.token = credential.accessToken;
+            this.setStoredToken(credential.accessToken);
             return true;
           }
 
@@ -45,10 +47,40 @@ export class AuthWrapperService {
   }
 
   isAuthenticated(): boolean {
-    return this.token !== '';
+    return this.getToken() !== '';
   }
 
   getToken(): string {
+    if (this.token === '') {
+      this.token = this.getStoredToken();
+    }
+
     return this.token;
+  }
+
+  private getStoredToken(): string {
+    let storedToken = localStorage.getItem(LOCAL_STORAGE_AUTH_NAME);
+    let storedTtl = localStorage.getItem(LOCAL_STORAGE_AUTH_TTL);
+
+    if (
+      storedToken !== undefined &&
+      storedToken !== null &&
+      storedTtl !== undefined &&
+      storedTtl !== null &&
+      parseInt(storedTtl) > Date.now()
+    ) {
+      return storedToken;
+    } else {
+      return '';
+    }
+  }
+
+  private setStoredToken(token: string): void {
+    localStorage.setItem(LOCAL_STORAGE_AUTH_NAME, token);
+    localStorage.setItem(LOCAL_STORAGE_AUTH_TTL, this.getOAuthExpiry());
+  }
+
+  private getOAuthExpiry(): string {
+    return Date.now() + 60000 + '';
   }
 }
